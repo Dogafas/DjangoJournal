aed02749fb99699477e7d7bdcc0ddc45ebab08a2
ByteBard <solomindavid10@gmail.com>
2024-10-27 16:04:35 +0700

    commit chapter 2

--- dblog/blog/models.py
+++ dblog/blog/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
-from django.utils import timezone
 from django.urls import reverse
+from django.utils import timezone
 
 
 class PublishedManager(models.Manager):
@@ -10,13 +10,17 @@ class PublishedManager(models.Manager):
             super().get_queryset().filter(status=Post.Status.PUBLISHED)
         )
 
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish'
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -25,30 +29,55 @@ class Post(models.Model):
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=2,
         choices=Status,
         default=Status.DRAFT
     )
-    objects = models.Manager()
-    published = PublishedManager()
+
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish']),
-            ]
-
+        ]
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         return reverse(
             'blog:post_detail',
-            args=[self.publish.year,
-                  self.publish.month,
-                  self.publish.day,
-                  self.slug]
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
